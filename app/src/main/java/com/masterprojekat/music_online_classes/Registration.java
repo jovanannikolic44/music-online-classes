fb0859b56c211caa8892ee7cce49405e7f9298fe
Jovana Nikolic <jovana.nikolic452@gmail.com>
2025-01-26 14:32:30 +0100

    Get data from radio buttons and spinner

--- app/src/main/java/com/masterprojekat/music_online_classes/Registration.java
+++ app/src/main/java/com/masterprojekat/music_online_classes/Registration.java
@@ -3,9 +3,12 @@ package com.masterprojekat.music_online_classes;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -32,11 +35,11 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 
-// *** NOTES: Layout scrollable, text colour to be black
 public class Registration extends AppCompatActivity {
 
     final Calendar calendar = Calendar.getInstance();
     EditText input_date;
+    private String expertise = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,22 +48,48 @@ public class Registration extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         showDateSpinner();
+        showExpertiseSpinner();
 
         register_new_user();
     }
 
     private void register_new_user() {
-        EditText inputName = findViewById(R.id.input_name);
-        EditText inputSurname = findViewById(R.id.input_surname);
-        EditText inputUsername = findViewById(R.id.input_username);
-        EditText inputPassword = findViewById(R.id.input_password);
-        EditText inputDate = findViewById(R.id.input_date);
-        EditText inputEmail = findViewById(R.id.input_email);
-        EditText inputPhoneNumber = findViewById(R.id.input_phone_number);
-        RadioGroup inputType = findViewById(R.id.radio_type);
-        EditText inputEducation = findViewById(R.id.input_education);
-        Spinner inputExpertise = findViewById(R.id.input_expertise);
-        Button registrationButton = findViewById(R.id.registration_button);
+        EditText inputName = (EditText) findViewById(R.id.input_name);
+        EditText inputSurname = (EditText) findViewById(R.id.input_surname);
+        EditText inputUsername = (EditText) findViewById(R.id.input_username);
+        EditText inputPassword = (EditText) findViewById(R.id.input_password);
+        EditText inputDate = (EditText) findViewById(R.id.input_date);
+        EditText inputEmail = (EditText) findViewById(R.id.input_email);
+        EditText inputPhoneNumber = (EditText) findViewById(R.id.input_phone_number);
+        RadioGroup inputType = (RadioGroup) findViewById(R.id.radio_type);
+        EditText inputEducation = (EditText) findViewById(R.id.input_education);
+        Spinner inputExpertise = (Spinner) findViewById(R.id.input_expertise);
+        Button registrationButton = (Button) findViewById(R.id.registration_button);
+
+        inputExpertise.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                expertise = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                expertise = "Nije selektovan";
+            }
+        });
+
+        inputType.setOnCheckedChangeListener((group, checkedId) -> {
+            RadioButton selectedButton = (RadioButton) findViewById(checkedId);
+            String type = String.valueOf(selectedButton.getText());
+            Toast.makeText(Registration.this, "RadioButton " + type, Toast.LENGTH_SHORT).show();
+            if ("Profesor".equals(type)) {
+                inputEducation.setVisibility(View.VISIBLE);
+                inputExpertise.setVisibility(View.VISIBLE);
+            } else {
+                inputEducation.setVisibility(View.GONE);
+                inputExpertise.setVisibility(View.GONE);
+            }
+        });
 
         RetrofitService retrofitService = new RetrofitService();
         UserAPI userApi = retrofitService.getRetrofit().create(UserAPI.class);
@@ -73,24 +102,22 @@ public class Registration extends AppCompatActivity {
             String date = String.valueOf(inputDate.getText());
             String email = String.valueOf(inputEmail.getText());
             String phoneNumber = String.valueOf(inputPhoneNumber.getText());
-            // radio button
-            String type = "ucenik";
+            RadioButton typeRadioButton = (RadioButton) findViewById(inputType.getCheckedRadioButtonId());
+            String type = String.valueOf(typeRadioButton.getText());
             String education = String.valueOf(inputEducation.getText());
-            // spinners
-            String expertise = "";
 
             User user = new User(name, surname, username, password, date, email, phoneNumber, type, education, expertise);
 
             userApi.saveUser(user).enqueue(new Callback<User>() {
                 @Override
                 public void onResponse(@NonNull Call<User> call, @NonNull Response<User> response) {
-                    Toast.makeText(Registration.this, "User saved successfully!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Registration.this, "Zahtev za registraciju uspesno poslat!", Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
                 public void onFailure(@NonNull Call<User> call, @NonNull Throwable throwable) {
                     Toast.makeText(Registration.this, "User saving failed!", Toast.LENGTH_SHORT).show();
-                    Logger.getLogger(Registration.class.getName()).log(Level.SEVERE, "Error occurred!", throwable);
+                    Logger.getLogger(Registration.class.getName()).log(Level.SEVERE, "Greska!", throwable);
                 }
             });
         });
@@ -115,5 +142,12 @@ public class Registration extends AppCompatActivity {
                 }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+    }
+
+    private void showExpertiseSpinner() {
+        Spinner inputExpertiseSpinner = (Spinner) findViewById(R.id.input_expertise);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.expertise_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        inputExpertiseSpinner.setAdapter(adapter);
     }
 }
