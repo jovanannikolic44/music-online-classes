102c4d644d72a3dadbcf3fa6c2a40c588505ab9c
Jovana Nikolic <jovana.nikolic452@gmail.com>
2025-02-02 23:50:39 +0100

    Preferences logic

--- app/src/main/java/com/masterprojekat/music_online_classes/Preferences.java
+++ app/src/main/java/com/masterprojekat/music_online_classes/Preferences.java
@@ -1,10 +1,16 @@
 package com.masterprojekat.music_online_classes;
 
+import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,9 +22,12 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.chip.Chip;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Preferences extends AppCompatActivity {
+    private final Set<String> clickedInstruments = new HashSet<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +40,76 @@ public class Preferences extends AppCompatActivity {
         int statusBarColor = ContextCompat.getColor(this, R.color.black);
         window.setStatusBarColor(statusBarColor);
 
-        setInstrumentsInLayout();
+        captureClickedPreferences();
     }
 
-    private void setInstrumentsInLayout() {
-//        GridLayout instrumentsLayout = findViewById(R.id.instruments_layout);
-//        List<String> instruments = Arrays.asList("Elektricna gitara", "Akusticna gitara", "Violina",
-//                "Harfa", "Klavir", "Harmonika", "Truba", "Saksofon", "Bubanj", "Pevanje");
-//
-//        for (String instrument : instruments) {
-//            Chip chip = new Chip(this);
-//            chip.setText(instrument);
-//            chip.setCheckable(true);
-//            chip.setChipCornerRadius(40);
-//            chip.setMinHeight(80);
-//            chip.setTextSize(18);
-//            chip.setPadding(24, 8, 24, 8);
-//            instrumentsLayout.addView(chip);
-//        }
+    private void captureClickedPreferences() {
+        GridLayout instrumentsLayout = findViewById(R.id.instruments_layout);
+
+        for(int i = 0; i < instrumentsLayout.getChildCount(); i++) {
+            View view = instrumentsLayout.getChildAt(i);
+            if(view instanceof LinearLayout) {
+                LinearLayout linearLayout = (LinearLayout) view;
+
+                ImageView imageView = null;
+                TextView textView = null;
+                switch (i) {
+                    case 0:
+                        imageView = linearLayout.findViewById(R.id.guitar_image);
+                        textView = linearLayout.findViewById(R.id.guitar_text);
+                        break;
+                    case 1:
+                        imageView = linearLayout.findViewById(R.id.violin_image);
+                        textView = linearLayout.findViewById(R.id.violin_text);
+                        break;
+                    case 2:
+                        imageView = linearLayout.findViewById(R.id.piano_image);
+                        textView = linearLayout.findViewById(R.id.piano_text);
+                        break;
+                    case 3:
+                        imageView = linearLayout.findViewById(R.id.harmonica_image);
+                        textView = linearLayout.findViewById(R.id.harmonica_text);
+                        break;
+                    case 4:
+                        imageView = linearLayout.findViewById(R.id.trumpet_image);
+                        textView = linearLayout.findViewById(R.id.trumpet_text);
+                        break;
+                    case 5:
+                        imageView = linearLayout.findViewById(R.id.saxophone_image);
+                        textView = linearLayout.findViewById(R.id.saxophone_text);
+                        break;
+                    case 6:
+                        imageView = linearLayout.findViewById(R.id.drums_image);
+                        textView = linearLayout.findViewById(R.id.drums_text);
+                        break;
+                    case 7:
+                        imageView = linearLayout.findViewById(R.id.singing_image);
+                        textView = linearLayout.findViewById(R.id.singing_text);
+                        break;
+                }
+
+                if(imageView == null)
+                    return;
+                final String instrmentName = (String) imageView.getTag();
+                TextView finalTextView = textView;
+                imageView.setOnClickListener(v -> {
+                    if(clickedInstruments.contains(instrmentName)) {
+                        clickedInstruments.remove(instrmentName);
+                    }
+                    else {
+                        clickedInstruments.add(instrmentName);
+                    }
+
+                    if (finalTextView.getTypeface().isBold()) {
+                        finalTextView.setTypeface(null, Typeface.NORMAL);
+                        finalTextView.setTextSize(11);
+                    } else {
+                        finalTextView.setTypeface(null, Typeface.BOLD);
+                        finalTextView.setTextSize(14);
+                    }
+
+                });
+            }
+        }
     }
 }
