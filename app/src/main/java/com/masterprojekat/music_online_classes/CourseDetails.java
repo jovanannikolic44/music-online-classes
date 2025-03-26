552b5861df7527cecd5a1eacacebe199d12f6107
Jovana Nikolic <jovana.nikolic452@gmail.com>
2025-03-26 21:45:56 +0100

    Add course detail display on page

--- app/src/main/java/com/masterprojekat/music_online_classes/CourseDetails.java
+++ app/src/main/java/com/masterprojekat/music_online_classes/CourseDetails.java
@@ -1,6 +1,8 @@
 package com.masterprojekat.music_online_classes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CourseDetails extends AppCompatActivity {
+import com.masterprojekat.music_online_classes.models.Course;
+import com.masterprojekat.music_online_classes.models.User;
 
+public class CourseDetails extends AppCompatActivity {
+    // Ako je kurs kupljen onda ima i input polje za komentare
+    private User loggedInUser;
+    private Course courseToDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_course_details);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Intent intent = getIntent();
+        loggedInUser = (User) intent.getSerializableExtra("loggedInUser");
+        courseToDisplay = (Course) intent.getSerializableExtra("course");
+        if(loggedInUser == null || courseToDisplay == null)
+            return;
+        displayCourseDetails();
+    }
+
+    private void displayCourseDetails() {
+        // get all simple fields
+        TextView courseNameView = findViewById(R.id.course_name);
+        TextView courseProfessorView = findViewById(R.id.course_professor);
+        TextView courseLevelView = findViewById(R.id.course_level_value);
+        TextView courseInstrumentView = findViewById(R.id.course_instrument_value);
+        TextView courseRatingView = findViewById(R.id.course_rating_value);
+        TextView coursePriceView = findViewById(R.id.course_price_value);
+        TextView courseDescriptionView = findViewById(R.id.course_description_value);
     }
 }
